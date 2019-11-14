9452d9e250a398406eb8cecc082f4b4ea57038d8
lijinze <1528562348@qq.com>
2019-11-14 22:50:22 +0800

    格式化代码

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 //app.js
 App({
-//系统事件
-onLaunch: function () {//小程序初始化事件
-var that = this;
-//调用API从本地缓存中获取数据
-that.curid = wx.getStorageSync('curid') || that.curid;//API：获取本地缓存，若不存在设置为全局属性
-that.setlocal('curid', that.curid);//调用全局方法
-},
-
+  //系统事件
+  onLaunch: function () {//小程序初始化事件
+  var that = this;
+  //调用API从本地缓存中获取数据
+  that.curid = wx.getStorageSync('curid') || that.curid;//API：获取本地缓存，若不存在设置为全局属性
+  that.setlocal('curid', that.curid);//调用全局方法
+  },
 
   /*******************************************************/
-
-
-
-
-
 
   //自定义全局方法
   setlocal: function (id, val) {
