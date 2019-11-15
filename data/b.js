a8b375f41bf0a05b3397a2bbce5c4f78877dd6d0
lijinze <lijinze@qq.com>
2019-11-15 18:37:34 +0800

    更新代码

---
+++ data/b.js
@@ -0,0 +1,9 @@
+var b = {
+  "errno": 0,
+  "errmsg": "",
+  "data": {
+    "goodsCount": 239
+  }
+}
+
+export default b
---
+++ pages/index/index.js
@@ -0,0 +1,58 @@
+//获取应用实例
+const app = getApp()
+
+import a from '../../data/a.js'
+import b from '../../data/b.js'
+
+Page({
+  data: {
+    goodsCount: 0,
+    newGoods: [],
+    hotGoods: [],
+    topics: [],
+    brands: [],
+    floorGoods: [],
+    banner: [],
+    channel: []
+  },
+  onShareAppMessage: function() {
+    return {
+      title: 'NideShop',
+      desc: '仿网易严选微信小程序商城',
+      path: '/pages/index/index'
+    }
+  },
+
+  getIndexData: function() {
+    let that = this;
+    let res = a
+    that.setData({
+      newGoods: res.data.newGoodsList,
+      hotGoods: res.data.hotGoodsList,
+      topics: res.data.topicList,
+      brand: res.data.brandList,
+      floorGoods: res.data.categoryList,
+      banner: res.data.banner,
+      channel: res.data.channel
+    });
+  },
+  onLoad: function(options) {
+    this.getIndexData();
+    let res = b
+    this.setData({
+      goodsCount: res.data.goodsCount
+    });
+  },
+  onReady: function() {
+    // 页面渲染完成
+  },
+  onShow: function() {
+    // 页面显示
+  },
+  onHide: function() {
+    // 页面隐藏
+  },
+  onUnload: function() {
+    // 页面关闭
+  },
+})
